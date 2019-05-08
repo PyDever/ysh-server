d7a5fe941a49f3b3e471a7e15f3858368aea5fdc
PyDever <bobafett2021@hotmail.com>
2019-05-07 15:04:06 -1000

    added application

--- server.js
+++ server.js
@@ -9,23 +9,31 @@ const express = require('express'),
           session = require('express-session'),
           lusca = require('lusca')
 
-const bodyParser = require('body-parser')
+const body_parser = require('body-parser')
 const request = require('request')
 const nosql = require('nosql')
 const db = nosql.load('/server/database.nosql')
 
 app.use(session(session_config))
-app.use(lusca(firewall_config))
+//app.use(lusca(firewall_config))
 
 app.use(express.static('public'))
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(body_parser.urlencoded({ extended: true }))
 app.set('view engine', 'ejs')
 
 app.get('/', function (req, res) {
   res.render('index', {weather: null, error: null})
 })
 
-app.post('/', function (req, res) {})
+app.post('/', function (req, res) {
+	let application_content = {
+		organization: req.body.organization,
+		project: req.body.project,
+		club: req.body.club
+	}
+	console.log(application_content)
+	
+})
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
